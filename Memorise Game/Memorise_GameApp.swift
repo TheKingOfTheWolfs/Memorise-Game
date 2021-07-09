48dd6450fe5e166f998b788482e3b256d31dcf9e
Yura Dzhishkariani MAC <Dziyura@gmail.com>
2021-07-08 22:28:31 -0700

    Initial Commit

---
+++ Memorise Game/Memorise_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Memorise_GameApp.swift
+//  Memorise Game
+//
+//  Created by Yury Dzhishkariani on 7/8/21.
+//
+
+import SwiftUI
+
+@main
+struct Memorise_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
